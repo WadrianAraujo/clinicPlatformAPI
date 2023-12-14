f9aafd1237f4d24258eba864da648b59a724e731
Wadrian Araujo <83624647+WadrianAraujo@users.noreply.github.com>
2023-12-14 10:05:55 -0400

    init project

---
+++ src/main/java/br/com/waxsolutions/clinicPlatformAPI/ClinicPlatformApiApplication.java
@@ -0,0 +1,15 @@
+package br.com.waxsolutions.clinicPlatformAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicPlatformApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ClinicPlatformApiApplication.class, args);
+
+    }
+
+}
